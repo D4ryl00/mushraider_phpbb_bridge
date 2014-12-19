8f5e07275e194535d233f20b24f9ab4e2715476d
Julien Goret <julien.goret@atos.net>
2014-12-19 15:17:41 +0100

    Change language to reflect new mushraider versions

--- root/language/en/mods/info_acp_mushraider_bridge.php
+++ root/language/en/mods/info_acp_mushraider_bridge.php
@@ -25,14 +25,14 @@ if (empty($lang) || !is_array($lang))
 $lang = array_merge($lang, array(
     'ACP_MUSHRAIDER_BRIDGE'                 => 'MushRaider Bridge',
     'MUSHRAIDER_BRIDGE'                     => 'MushRaider Bridge',
-    'ACP_MUSHRAIDER_BRIDGE_CONFIG'          => 'Configuration',
+    'ACP_MUSHRAIDER_BRIDGE_CONFIG'          => 'Global',
     'ACP_MUSHRAIDER_BRIDGE_CONFIG_TITLE'    => 'MushRaider Bridge Configuration',
     'ACP_MUSHRAIDER_BRIDGE_PERMS'           => 'Permissions',
     'ACP_MUSHRAIDER_BRIDGE_PERMS_TITLE'     => 'MushRaider Permissions Configuration',
     
     'MUSHRAIDER_BRIDGE_ENABLE'              => 'Enable MushRaider Bridge?',
-    'MUSHRAIDER_BRIDGE_KEY'                 => 'MushRaider Bridge Secret Key',
-    'MUSHRAIDER_BRIDGE_KEY_EXPLAIN'         => 'This is the key you entered in MushRaider\'s configuration panel',
+    'MUSHRAIDER_BRIDGE_KEY'                 => 'MushRaider API Key',
+    'MUSHRAIDER_BRIDGE_KEY_EXPLAIN'         => 'This is the API key you generated in MushRaider\'s configuration panel',
     'MUSHRAIDER_BRIDGE_ADMINS'              => 'Administrators',
     'MUSHRAIDER_BRIDGE_ADMINS_EXPLAIN'      => 'List of phpBB groups which are Administrators of MushRaider /!\ Any groups not in the lists will not be allowed to connect to MushRaider /!\ ',
     'MUSHRAIDER_BRIDGE_OFFICERS'            => 'Officers',
--- root/language/fr/mods/info_acp_mushraider_bridge.php
+++ root/language/fr/mods/info_acp_mushraider_bridge.php
@@ -25,14 +25,14 @@ if (empty($lang) || !is_array($lang))
 $lang = array_merge($lang, array(
     'ACP_MUSHRAIDER_BRIDGE'                 => 'MushRaider Bridge',
     'MUSHRAIDER_BRIDGE'                     => 'MushRaider Bridge',
-    'ACP_MUSHRAIDER_BRIDGE_CONFIG'          => 'Configuration',
+    'ACP_MUSHRAIDER_BRIDGE_CONFIG'          => 'Global',
     'ACP_MUSHRAIDER_BRIDGE_CONFIG_TITLE'    => 'Configuration de MushRaider Bridge',
     'ACP_MUSHRAIDER_BRIDGE_PERMS'           => 'Permissions',
     'ACP_MUSHRAIDER_BRIDGE_PERMS_TITLE'     => 'Configuration des permissions de MushRaider',
     
     'MUSHRAIDER_BRIDGE_ENABLE'              => 'Activer le Bridge MushRaider?',
-    'MUSHRAIDER_BRIDGE_KEY'                 => 'Clef secrète du bridge MushRaider',
-    'MUSHRAIDER_BRIDGE_KEY_EXPLAIN'         => 'Il s\'agit de la clé que vous avez entré dans le panneau de configuration de MushRaider',
+    'MUSHRAIDER_BRIDGE_KEY'                 => 'Clef privée du bridge MushRaider',
+    'MUSHRAIDER_BRIDGE_KEY_EXPLAIN'         => 'Il s\'agit de la clé d\'API que vous avez géneré dans le panneau de configuration de MushRaider',
     'MUSHRAIDER_BRIDGE_ADMINS'              => 'Administrateurs',
     'MUSHRAIDER_BRIDGE_ADMINS_EXPLAIN'      => 'Liste des groupes phpBB considérés comme administrateurs de MushRaider /!\ Seuls les groupes appartenant a une de ces listes peut se connecter a MushRaider /!\ ',
     'MUSHRAIDER_BRIDGE_OFFICERS'            => 'Officiers',
